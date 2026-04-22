c49d27c7b1b7e4ed191f39a1fafee231efa656aa
MarioCamara <angelmarito1705@gmail.com>
2026-04-22 08:16:53 +0530

    mejoras de chatbot y pines del mapa

--- src/app/services/poi/poi.service.ts
+++ src/app/services/poi/poi.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Poi, PoiResponse } from 'src/app/models/poi.model';
+import { Poi } from 'src/app/models/poi.model';
 import { ApiService } from '../api/api.service';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +10,7 @@ export class PoiService {
   constructor(private apiService: ApiService) {}
 
   getAll(): Observable<Poi[]> {
-    return this.apiService.getData<PoiResponse>('/places/').pipe(
-      map(response => response.results)
-    );
+    return this.apiService.getData<Poi[]>('/places/');
   }
 
   getById(id: string): Observable<Poi> {
